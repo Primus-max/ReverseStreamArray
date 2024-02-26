4c287a3ceeb6ae8adaec3217c5ea20c2aa5aa44e
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-26 12:44:34 +0300

    Добавьте файлы проекта.

---
+++ ReverseStreamArray.cpp
@@ -0,0 +1,50 @@
+﻿// Написать функцию, меняющую порядок следования элементов передаваемого ей массива на противоположный.
+
+
+#include <iostream>
+#include <windows.h>
+#include <random>
+
+using namespace std;
+
+
+template <typename T>
+void FillArray(T arr[], int size);
+
+template <typename T>
+void PrintArray(T arr[], int size);
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	const int sizze = 30;
+	int Array[sizze];
+
+	FillArray(Array, sizze);
+	PrintArray(Array, sizze);
+	
+}
+
+
+template <typename T>
+void FillArray(T arr[], int size) {
+	random_device Random;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = Random() % (100 + 50) - 50;
+	}
+}
+
+template <typename T>
+void PrintArray(T arr[], int size) {
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+}
